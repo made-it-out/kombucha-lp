19e12a8cd666475d30d507a9276e107a3b0ba226
made-it-out <mike.hallisy@gmail.com>
2021-10-20 21:27:27 -0400

    updated nav responsiveness

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     // Header
+    const header = document.querySelector('.header');
     const nav = document.querySelector('.nav');
     const headerToggle = document.querySelector('.header__toggle');
 
@@ -7,16 +8,26 @@ window.addEventListener('DOMContentLoaded', () => {
     headerToggle.addEventListener('keydown', toggleKeyDown);
 
     function toggleClick(e) {
+        header.classList.toggle('nav-shown');
         nav.classList.toggle('shown');
         document.body.classList.toggle('modal-open');
     }
 
     function toggleKeyDown(e) {
         if (e.keyCode === 13 || e.keyCode === 32) {
+            header.classList.toggle('nav-shown');
             nav.classList.toggle('shown');
             document.body.classList.toggle('modal-open');
         }
     }
+
+    window.addEventListener('resize', () => {
+        if(window.innerWidth >= 768){
+            header.classList.remove('nav-shown');
+            nav.classList.remove('shown');
+            document.body.classList.remove('modal-open')
+        }
+    })
 
     // Testimonials
     const testimonailsSlides = Array.from(document.querySelectorAll('.testimonials__slide'));
@@ -74,7 +85,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Animations on scroll
     const animateOptions = {
-        threshold: 0
+        threshold: 0.1
     }
 
     const animateOnScroll = new IntersectionObserver(function (entries, animateOnScroll) {
